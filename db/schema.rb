1e6dcb455a8b8196999f92769ac82f0322c0370c
Roosvelt T <elpresidentemaestro@gmail.com>
2018-05-05 13:39:00 +0000

    Création de page utilisateur

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180505121726) do
+ActiveRecord::Schema.define(version: 20180505124053) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 20180505121726) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "phone_nuber"
+    t.string   "phone_number"
+    t.text     "description"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
